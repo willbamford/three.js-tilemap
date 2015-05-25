dcae132a2fefb8522175f262cab9f0548bde6e7e
Will Bamford <w.bamford@gmail.com>
2015-05-25 13:57:14 +0100

    Changes

---
+++ Gruntfile.js
@@ -0,0 +1,54 @@
+module.exports = function (grunt) {
+
+  grunt.initConfig({
+
+    pkg: grunt.file.readJSON('package.json'),
+    aws: grunt.file.readJSON('aws-auth.json'),
+    aws_s3: {
+      options: {
+        accessKeyId: '<%= aws.key %>', // Use the variables
+        secretAccessKey: '<%= aws.secret %>', // You can also use env variables
+        region: '<%= aws.region %>',
+        access: 'public-read',
+        debug: false,
+        uploadConcurrency: 5, // 5 simultaneous uploads
+        downloadConcurrency: 5 // 5 simultaneous downloads
+      },
+      production: {
+        options: {
+          bucket: '<%= aws.bucket %>',
+          differential: true // Only uploads the files that have changed
+        },
+
+        files: [
+          {
+            src: 'index.html',
+            dest: 'index.html'
+          },
+          {
+            src: 'led.png',
+            dest: 'led.png'
+          },
+          {
+            src: 'node_modules/stats.js/build/stats.min.js',
+            dest: 'node_modules/stats.js/build/stats.min.js'
+          },
+          {
+            src: 'three.js/build/three.js',
+            dest: 'three.js/build/three.js'
+          },
+          {
+            expand: true,
+            cwd: 'src',
+            src: ['**'],
+            dest: 'src'
+          }
+        ]
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-aws-s3');
+
+  grunt.registerTask('default', ['aws_s3']);
+};
--- src/app.js
+++ src/app.js
@@ -1,14 +1,10 @@
 function initStats() {
   var stats = new Stats();
   stats.setMode(0); // 0: fps, 1: ms
-
-  // align top-left
   stats.domElement.style.position = 'absolute';
   stats.domElement.style.left = '0px';
   stats.domElement.style.top = '0px';
-
   document.body.appendChild(stats.domElement);
-
   return stats;
 }
 
@@ -16,24 +12,35 @@ var stats = initStats();
 
 var scene = new THREE.Scene();
 
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
-camera.position.z = 1000;
+var camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 1, 3000);
+camera.position.z = 1400;
 
-// var geometry = new THREE.BoxGeometry(200, 200, 200);
-// var material = new THREE.MeshBasicMaterial({color: 0xff0000, wireframe: true});
-// var mesh = new THREE.Mesh(geometry, material);
-// scene.add(mesh);
+var geometry = new THREE.Geometry();
+var colors = [];
+var sprite = THREE.ImageUtils.loadTexture('led.png');
 
-var geometry = new THREE.PlaneGeometry(1000, 1000, 2, 2);
-var material = new THREE.MeshBasicMaterial({color: 0xffff00, side: THREE.DoubleSide, wireframe: true});
-var plane = new THREE.Mesh(geometry, material);
-scene.add(plane);
+for (var i = 0; i < 1000; i += 1) {
+  var vertex = new THREE.Vector3();
+  vertex.x = 2000 * Math.random() - 1000;
+  vertex.y = 2000 * Math.random() - 1000;
+  vertex.z = 2000 * Math.random() - 1000;
+  geometry.vertices.push(vertex);
 
-window.plane = plane;
+  colors[i] = new THREE.Color(0xffffff);
+  colors[i].setHSL(Math.random(), 1, 0.5);
+}
 
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+geometry.colors = colors;
+
+var material = new THREE.PointCloudMaterial({size: 85, map: sprite, blending: THREE.AdditiveBlending, vertexColors: THREE.VertexColors, depthTest: false, transparent: true});
+// material.color.setHSL(1.0, 1.0, 1.0);
+
+var particles = new THREE.PointCloud(geometry, material);
+scene.add(particles);
+
+
+var lastNow;
+var delta = 0;
 
 function update() {
 
@@ -41,12 +48,31 @@ function update() {
 
   stats.begin();
 
-  plane.rotation.x += 0.001;
-  plane.rotation.y += 0.002;
+  now = Date.now();
+  delta = lastNow ? now - lastNow : (1000 / 60);
+  lastNow = now;
+
+  particles.rotation.x += delta * 0.0001;
+  particles.rotation.y += delta * 0.0002;
 
   renderer.render(scene, camera);
 
   stats.end();
 }
+
+function onResize(event) {
+  var w = window.innerWidth;
+  var h = window.innerHeight;
+  camera.aspect = w / h;
+  camera.updateProjectionMatrix();
+  renderer.setSize(w, h);
+}
+
+var renderer = new THREE.WebGLRenderer();
+// renderer.setPixelRatio(window.devicePixelRatio || 1);
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+window.addEventListener('resize', onResize, false);
 
 update();
